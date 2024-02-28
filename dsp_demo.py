3f6e6aad1a261246026c5c0cc629f929c55d3dc3
Mihir Katyal <mkatyal@uci.edu>
2024-02-27 21:40:39 -0800

    updated code

--- dsp_demo.py
+++ dsp_demo.py
@@ -1,8 +1,6 @@
+# dsp_demo.py
+
 import ds_client
-
-server = "168.235.86.101 "  # replace with actual server ip address
-port = 3021   # replace with actual port
-
-# Add more realistic credentials and messages
-demo_results = ds_client.send(server, port, "demo_user", "demo_pass", "Hello, this is a demo message!", "This is a demo bio.")
-print("Test Results:", demo_results)
+server = "168.235.86.101" 
+port = 3021  
+ds_client.send(server, port, "f21demo", "pwd123", "Hello World!")
