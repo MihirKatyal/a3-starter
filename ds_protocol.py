17b1edf257774c555df3372ffe54ad3255bc67f6
Mihir Katyal <mkatyal@uci.edu>
2024-02-27 19:43:49 -0800

    added error tuple

--- ds_protocol.py
+++ ds_protocol.py
@@ -1,19 +1,11 @@
-# ds_protocol.py
-
-# Starter code for assignment 3 in ICS 32 Programming with Software Libraries in Python
-
-# Replace the following placeholders with your information.
-
-# NAME
-# EMAIL
-# STUDENT ID
-
 import json
 from collections import namedtuple
 
 # Namedtuple to hold the values retrieved from json messages.
 # TODO: update this named tuple to use DSP protocol keys
 DataTuple = namedtuple('DataTuple', ['foo','baz'])
+
+ErrorTuple = namedtuple('ErrorTuple', ['type', 'message', 'token'])
 
 def extract_json(json_msg:str) -> DataTuple:
   '''
